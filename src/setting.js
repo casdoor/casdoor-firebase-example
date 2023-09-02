aba8b8d2a73364bbb3f3f4402d50a4b22190a4c5
Yaodong Yu <2814461814@qq.com>
2023-09-02 10:47:15 +0800

    feat: add firebase connect casdoor example (#2)
    * feat: add firebase connect casdoor example
    
    * feat: improve README.md

---
+++ src/setting.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "×××",
+  authDomain: "your-project-name.firebaseapp.com",
+  projectId: "your-project-name",
+  storageBucket: "your-project-name.appspot.com",
+  messagingSenderId: "×××",
+  appId: "×××",
+  measurementId: "×××"
+};
+
+export const app = initializeApp(firebaseConfig);
